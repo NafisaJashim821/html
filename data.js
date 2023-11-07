8b59732c6106ecc0efc52652cf6667cccc602405
nafisa <nafisa@jashim.com>
2023-11-07 20:55:25 +0600

    b tag

---
+++ data.js
@@ -0,0 +1,10 @@
+var date = new Date();
+console.log(date);
+
+var day = date.getDate();
+console.log(day);
+var head = document.getElementById("heading1");
+
+head.innerHTML = "Hello";
+document.getElementById("para");
+document.getElementsByClassName("hey");
